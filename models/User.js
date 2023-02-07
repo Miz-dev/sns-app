ecb72b3f225a79d07714919a876c71afe56922c1
shn <topdog.shn.01@gmail.com>
2023-02-08 00:07:41 +0900

    ✨ ユーザースキーマのモデル作成

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			min: 3,
+			max: 25,
+			uniqque: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			max: 50,
+			uniqque: true,
+		},
+		passward: {
+			type: String,
+			required: true,
+			min: 6,
+			max: 50,
+			uniqque: true,
+		},
+		profilePicture: {
+			type: String,
+			default: "",
+		},
+		coverPicture: {
+			type: String,
+			default: "",
+		},
+		followers: {
+			type: Array,
+			default: "",
+		},
+		followings: {
+			type: Array,
+			default: "",
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+		desc: {
+			type: String,
+			max: 70,
+		},
+		city: {
+			type: String,
+			max: 50,
+		},
+	},
+
+	{ timestamps: true }
+);
