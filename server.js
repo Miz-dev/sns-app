92ce39bd3f0292a8be7b429c9affa3896d949e40
shn <topdog.shn.01@gmail.com>
2023-02-08 00:32:11 +0900

    ✨ MongoDBとNode.js接続

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-//ミドルウェア
+// データベース接続
+mongoose
+	.connect(process.env.MONGOURL)
+	.then(() => {
+		console.log("DBと接続中・・・");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+// ミドルウェア
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
