00c230bcf9927fefd1c42d8dec87e78d5d8ab70b
shn <topdog.shn.01@gmail.com>
2023-02-12 23:56:26 +0900

    ✨ タイムラインの投稿を取得するAPI作成

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // 投稿を作成する
 router.post("/", async (req, res) => {
@@ -73,6 +74,23 @@ router.put("/:id/like", async (req, res) => {
 		}
 	} catch (err) {
 		return res.status(500).json(err);
+	}
+});
+
+// タイムラインの投稿を取得する
+router.get("/timeline/all", async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.params.userId);
+		const userPosts = await Post.find({ userId: currentUser._id });
+		// 自分がフォローしている友達の投稿内容を全て取得する
+		const friendPosts = await Promise.all(
+			currentUser.followings.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+		res.status(200).json(userPosts.concat(...friendPosts));
+	} catch (err) {
+		res.status(500).json(err);
 	}
 });
 
