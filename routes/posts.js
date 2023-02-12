8e3a3cb17638077aac49a18e015fd4e78a5ba0b4
shn <topdog.shn.01@gmail.com>
2023-02-12 21:49:37 +0900

    ✨ 投稿を編集するAPIを作成

--- routes/posts.js
+++ routes/posts.js
@@ -13,4 +13,22 @@ router.post("/", async (req, res) => {
 	}
 });
 
+// 投稿を更新する
+router.put("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (post.userId === req.body.userId) {
+			await post.updateOne({
+				$set: req.body,
+			});
+			return res.status(200).json("投稿編集に成功しました！");
+		} else {
+			return res.status(403).json("あなたは他人の投稿を編集できません");
+		}
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+});
+
 module.exports = router;
